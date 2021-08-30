c832c13ca180fb5c1c8ec68c08f67023ed206a6f
qti <84819214+qu3tiiii@users.noreply.github.com>
2021-08-30 18:06:41 -0400

    Create clck.py

---
+++ clck.py
@@ -0,0 +1,25 @@
+import pyautogui, time, os
+os.system('Title Clicker!')
+print("""
+   ,d88b
+ ,8P'    `8,
+ 8'       _.8._
+8       .'  |  '.
+       /    |     |
+      |    [_]    |
+      |     |     |
+      |-----'-----|
+      |           |
+      |           |
+      |;         .|
+      ;\         /;
+       \        //
+        '._ _.'/
+         '-...-'
+github: https://github.com/qu3tiiii
+:)
+""")
+xz = int(input("Cantidad de clicks: "))
+for i in range(xz):
+     pyautogui.click()
+
